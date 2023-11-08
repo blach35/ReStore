5fc13b17eff4c71ad2202ace0241215dfd60abb2
blach35 <koraykaragoz@hotmail.com.tr>
2023-11-08 17:15:21 +0300

    End of suction 4

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -10,8 +10,8 @@ export  default function ProductList({products}: Props){
     return(
         <Grid container spacing={4}>
         {products.map(product => (
-            <Grid item xs={3} >
-                 <ProductCard product={product}></ProductCard>
+            <Grid item xs={3}  >
+                 <ProductCard product={product} ></ProductCard>
             </Grid>
            
         ))}
